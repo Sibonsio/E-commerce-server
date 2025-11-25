52394ce5d92cffb79a4e29c0d7fc1f7b094a0c55
Sibonsio <siboniso1sekute@gmail.com>
2025-11-25 12:02:52 +0200

    added cors middleware

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import cors from 'cors'
 import connectDB from './src/config/db.js'
 import errorMiddleware from './src/middleware/errorMiddleware.js'
 import router from './src/routes/authRoutes.js'
+import corsOptions from './src/config/cors.js'
 
 const app = express()
 dotenv.config({ path: ".env" })
 await connectDB()
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(errorMiddleware)
